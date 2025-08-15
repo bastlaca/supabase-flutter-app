c769c5c0af4041da4f228036ff61ce0666626d8f
Básti László <bastlaca@gmail.com>
2025-08-15 22:59:37 +0200

    fix container issues (#1)
    * fix container issues
    
    * improve logged-in shell navigation

--- lib/feature/login/login_screen.dart
+++ lib/feature/login/login_screen.dart
@@ -7,36 +7,35 @@ class LoginScreen extends ConsumerWidget {
 
   @override
   @override
-Widget build(BuildContext context, WidgetRef ref) {
-  return GestureDetector(
-    onTap: () => FocusScope.of(context).unfocus(),
-    behavior: HitTestBehavior.opaque,
-    child: Scaffold(
-      appBar: AppBar(title: const Text('Login screen')),
-      body: SizedBox.expand(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(minHeight: constraints.maxHeight),
-                child: Center( // most már tényleg középre tud igazítani
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: SupaEmailAuth(
-                      onSignInComplete: _onSignInComplete,
-                      onSignUpComplete: _onSignUpComplete,
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      behavior: HitTestBehavior.opaque,
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Login screen')),
+        body: SizedBox.expand(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SupaEmailAuth(
+                        onSignInComplete: _onSignInComplete,
+                        onSignUpComplete: _onSignUpComplete,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
-    ),
-  );
-}
-
+    );
+  }
 
   void _onSignInComplete(AuthResponse response) {}
 
